c1ba334d0665178b4a7d7097facd163c9ecd5fb7
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-02-08 13:04:42 +0300

    fix import

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
